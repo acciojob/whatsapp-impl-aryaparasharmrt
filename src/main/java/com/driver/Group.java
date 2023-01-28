8ebe9a082d992112e279610952d48589b39b581c
ASUS <aryaprasharj02@gmail.com>
2023-01-28 23:29:52 +0530

    Solution 3

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -4,7 +4,7 @@ public class Group {
     private String name;
     private int numberOfParticipants;
 
-    Group(String name, int numberOfParticipants){
+    public Group(String name, int numberOfParticipants){
         this.name = name;
         this.numberOfParticipants = numberOfParticipants;
     }
